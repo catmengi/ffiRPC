d5c80ca8b0fd89e48649a1794de5813944cc8260
Catmengi <catengineerforever@gmail.com>
2025-04-22 22:35:39 +0300

    Moved functions out of header. Documentation on set/get macros for future

--- ffiRPC_struct.c
+++ ffiRPC_struct.c
@@ -1,18 +1,34 @@
 #include "ffiRPC_struct.h"
-#include <assert.h>
 
-#include <stdio.h>
+ffiRPC_struct_t ffiRPC_struct_create(void){
+    ffiRPC_struct_t ffiRPC_struct = (ffiRPC_struct_t)malloc(sizeof(*ffiRPC_struct));
+    assert(ffiRPC_struct);
 
+    ffiRPC_struct->ht = hashtable_create();
+    ffiRPC_struct->size = 0;
+
+    return ffiRPC_struct;
+}
+
+int ffiRPC_is_pointer(enum ffiRPC_types type){
+    int ret = 0;
+    if(type == ffiRPC_struct || type == ffiRPC_string || type == ffiRPC_unknown) ret = 1;
+
+    return ret;
+}
+void ffiRPC_container_free(struct ffiRPC_container_element* element){
+
+}
+
+
+//REMOVE WHEN DONE!
 int main(){
     ffiRPC_struct_t ffiRPC_struct = ffiRPC_struct_create();
-    char str[32];
-    for(size_t i = 0; i < 512; i++){
-        sprintf(str,"%zu",i);
-        ffiRPC_struct_set(ffiRPC_struct,str,(char*)str);
-    }
-    ffiRPC_struct_set(ffiRPC_struct,(char*)"check_int",(uint64_t)1234567);
-    char* ptr;uint64_t output;
-    assert(ffiRPC_struct_get(ffiRPC_struct,"62",ptr) == 0);
+
+    uint64_t input = 12345678;
+    ffiRPC_struct_set(ffiRPC_struct,"check_int",input);
+    uint64_t output;
     assert(ffiRPC_struct_get(ffiRPC_struct,"check_int",output) == 0);
-    puts(ptr); printf("%lu check\n",output);
+    assert(output == input);
 }
+//=================
--- ffiRPC_struct.h
+++ ffiRPC_struct.h
@@ -34,7 +34,12 @@ struct ffiRPC_struct{
     hashtable* ht;
     atomic_size_t size;
 };
+typedef struct ffiRPC_struct *ffiRPC_struct_t;
 
+ffiRPC_struct_t ffiRPC_struct_create(void);
+
+int ffiRPC_is_pointer(enum ffiRPC_types type);
+void ffiRPC_container_free(struct ffiRPC_container_element* element);
 
 #define CType_to_ffiRPC(Native_type) _Generic((Native_type),                   \
                                     char                 : ffiRPC_char,        \
@@ -52,28 +57,6 @@ struct ffiRPC_struct{
                                     ffiRPC_struct_t      : ffiRPC_struct,      \
                                     default              : ffiRPC_unknown      \
 )
-
-
-static int ffiRPC_is_pointer(enum ffiRPC_types type){
-    int ret = 0;
-    if(type == ffiRPC_struct || type == ffiRPC_string || type == ffiRPC_unknown) ret = 1;
-
-    return ret;
-}
-
-
-typedef struct ffiRPC_struct *ffiRPC_struct_t;
-
-
-static inline ffiRPC_struct_t ffiRPC_struct_create(){
-    ffiRPC_struct_t ffiRPC_struct = (ffiRPC_struct_t)malloc(sizeof(*ffiRPC_struct));
-    assert(ffiRPC_struct);
-
-    ffiRPC_struct->ht = hashtable_create();
-    ffiRPC_struct->size = 0;
-
-    return ffiRPC_struct;
-}
 
 #define ffiRPC_cast_value(output, input) typeof(output) cpy = input; output = cpy;
 
@@ -95,12 +78,30 @@ static inline ffiRPC_struct_t ffiRPC_struct_create(){
     }})
 
 
-#define ffiRPC_struct_set(ffiRPC_struct, key, type)({\
-    struct ffiRPC_container_element* element = malloc(sizeof(*element)); assert(element);\
-    C_to_ffiRPC(element,type);\
+/*Sets a structure element at ffiRPC_struct with type of "input" and value of "input".
+ *NOTE: If you are passing string literal you SHOULD cast it to char*
+ *NOTE: strings(char*) are always copied when passing into structure
+ *NOTE: you can pass void* or any other pointer into the struct but it WONT be serialised
+ *NOTE: "key" are always copied when passing into structure
+ *
+ *EXAMPLE: ffiRPC_struct_set(ffiRPC_struct,"check_int",(uint64_t)12345678);
+*/
+#define ffiRPC_struct_set(ffiRPC_struct, key, input)({\
+    struct ffiRPC_container_element* element = hashtable_get(ffiRPC_struct->ht,key);\
+    if(element == NULL) {element = malloc(sizeof(*element)); assert(element); ffiRPC_struct->size++;}\
+    else{ffiRPC_container_free(element);}\
+    C_to_ffiRPC(element,input);\
     hashtable_set(ffiRPC_struct->ht,strdup(key),element);\
-    ffiRPC_struct->size++;(int)(0);})
+    (int)(0);})
 
-
+/*Get element from ffiRPC_struct at key "key" and writes it's data into "output". If element "key" does NOT exist returns 1 else 0
+ *NOTE: you SHOULD type only NAME of output variable into "output", NOT &output !!!
+ *
+ *EXAMPLE: uint64_t input = 12345678;
+ *         ffiRPC_struct_set(ffiRPC_struct,"check_int",input);
+ *         uint64_t output;
+ *         assert(ffiRPC_struct_get(ffiRPC_struct,"check_int",output) == 0);
+ *         assert(output == input);
+*/
 #define ffiRPC_struct_get(ffiRPC_struct, key, output)({int ret = 1;struct ffiRPC_container_element* element = hashtable_get(ffiRPC_struct->ht,key);\
                                                     if(element != NULL){assert(element->type == CType_to_ffiRPC(output));if(ffiRPC_is_pointer(element->type)){ffiRPC_cast_value(output,(typeof(output))element->data);} else{ffiRPC_cast_value(output,*(typeof(output)*)element->data);} ret = 0;}(ret);})
